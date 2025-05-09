bb6f5de4286403e788ca5a50a31aa0600f120f8f
Segun228 <segunperkele@gmail.com>
2025-05-09 08:18:11 +0300

    adding search bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import AnimatedSearchField from './components/animatedSearchField/AnimatedSearchField'
+import SearchBar from './components/search_bar_react_template/src/components/SearchBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='componentContainer'>
       <AnimatedSearchField></AnimatedSearchField>
-    </>
+      <SearchBar></SearchBar>
+    </div>
   )
 }
 
