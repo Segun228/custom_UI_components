fbcf8f23cdd2162072ba5455b87ac27add23d22c
Segun228 <segunperkele@gmail.com>
2025-05-09 16:25:41 +0300

    register and login form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import ActionButton from './components/actionButton/ActionButton'
 import AnimatedSearchField from './components/animatedSearchField/AnimatedSearchField'
 import AnswerField from './components/answerField/AnswerField.jsx'
+import LoginForm from './components/logInForm/LoginForm.jsx'
+import RegisterForm from './components/regForm/RegForm.jsx'
 import SearchField from './components/searchField/SearchField'
 import Stars from './components/stars/Stars.jsx'
 
@@ -14,6 +16,8 @@ function App() {
       <SearchField></SearchField>
       <Stars rating={3}></Stars>
       <AnswerField></AnswerField>
+      <RegisterForm></RegisterForm>
+      <LoginForm></LoginForm>
     </div>
   )
 }
